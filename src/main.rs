351f48e928002a0ffa4895dba0896ee8a527dca4
Vignesh P <vignesh.p1110@gmail.com>
2020-07-01 22:12:43 +0530

    accepting user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    println!("Please enter the sudoku puzzle:");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input!");
+
+    println!("Your input was : {}", input);
 }
