81a2291303ad2b26e7dff007560d63ef9ae62cc5
Vignesh P <vignesh.p1110@gmail.com>
2020-07-03 23:51:13 +0530

    validated input character set

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,33 @@ fn main() {
     println!("Hello, world!");
 
     println!("Please enter the sudoku puzzle:");
-    let mut input = String::new();
+    let mut raw_input = String::new();
+    let digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     io::stdin()
-        .read_line(&mut input)
+        .read_line(&mut raw_input)
         .expect("Failed to read input!");
 
-    println!("Your input was : {}", input.trim().chars().count());
+    let input = raw_input.trim();
+    let input_length = input.chars().count();
+    println!("Your input was : {}", input_length);
 
-    match 81.cmp(&input.trim().chars().count()) {
+    match 81.cmp(&input_length) {
         Ordering::Less => println!("This seems too long!"),
         Ordering::Greater => println!("You haven't typed the full puzzle!"),
         Ordering::Equal => println!("Validating input..."),
+    }
+    let char_vec: Vec<char> = input.chars().collect();
+    let mut error_flag: bool = false;
+    for c in char_vec {
+        if digits.contains(&c) {
+            continue;
+        } else {
+            println!("{} is an invalid entry in the input!", c);
+            error_flag = true;
+            break;
+        }
+    }
+    if error_flag == false {
+        println!("Input characters seem valid");
     }
 }
